bb2f8bed7d8a21251f264bf0ac461e547334d195
Saleel Salam <saleel97@gmail.com>
2022-07-24 12:23:58 +0100

    Replace forking with fromId

--- src/pages/dashboard-page.js
+++ src/pages/dashboard-page.js
@@ -1,23 +1,17 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Dashboard from '../components/dashboard';
-import { getDashboard, saveDashboardLocally } from '../data-service';
+import { getDashboard } from '../data-service';
 import usePromise from '../hooks/use-promise';
 
 function DashboardPage() {
-  const navigate = useNavigate();
   const { dashboardId } = useParams();
 
   const [dashboard, { isFetching, error }] = usePromise(() => getDashboard(dashboardId), {
     dependencies: [dashboardId],
     conditions: [dashboardId],
   });
-
-  const onForkClick = React.useCallback(async () => {
-    await saveDashboardLocally(dashboard);
-    navigate('/dashboard/new'); // TODO: a hack for now - new widget page will load the most recent local widget
-  }, [dashboard]);
 
   if (isFetching) {
     return (<div className="page dashboard-page">Loading</div>);
@@ -36,13 +30,13 @@ function DashboardPage() {
         </h2>
 
         <div className="flex-row">
-          <a className="link view-source mr-2 pt-1" href={`https://ipfs.io/ipfs/${dashboardId}`} target="_blank" rel="noreferrer" title="View source">
+          <a className="link mr-2 pt-1" href={`https://ipfs.io/ipfs/${dashboardId}`} target="_blank" rel="noreferrer" title="View source">
             <i className="icon-code" />
           </a>
 
-          <div role="button" tabIndex={0} className="icon-button pt-1" onClick={onForkClick} title="Copy this dashbord locally and edit">
+          <Link className="link icon-button pt-1" to={`/dashboard/new?fromId=${dashboardId}`} title="Copy this dashbord locally and edit">
             <i className="icon-clone" />
-          </div>
+          </Link>
         </div>
       </div>
 
--- src/pages/widget-page.js
+++ src/pages/widget-page.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Widget from '../components/widget';
-import { getWidget, saveWidgetLocally } from '../data-service';
+import { getWidget } from '../data-service';
 import usePromise from '../hooks/use-promise';
 
 function WidgetPage() {
-  const navigate = useNavigate();
   const { widgetId } = useParams();
 
   const [widgetConfig, { isFetching, error }] = usePromise(() => getWidget(widgetId), {
     dependencies: [widgetId],
     conditions: [widgetId],
   });
-
-  const onForkClick = React.useCallback(async () => {
-    await saveWidgetLocally(widgetConfig);
-    navigate('/widget/new'); // TODO: a hack for now - new widget page will load the most recent local widget
-  }, [widgetConfig]);
 
   if (isFetching) {
     return (<div>Loading</div>);
@@ -32,25 +26,17 @@ function WidgetPage() {
       <div className="widget-actions">
         <div />
         <div className="flex-row">
-          <a className="link view-source mr-2 pt-1" href={`https://ipfs.io/ipfs/${widgetId}`} target="_blank" rel="noreferrer" title="View source">
+          <a className="link mr-2 pt-1" href={`https://ipfs.io/ipfs/${widgetId}`} target="_blank" rel="noreferrer" title="View source">
             <i className="icon-code" />
           </a>
 
-          <div role="button" tabIndex={0} className="icon-button pt-1" onClick={onForkClick} title="Copy this dashbord locally and edit">
+          <Link className="link icon-button pt-1" to={`/widget/new?fromId=${widgetId}`} title="Copy this dashbord locally and edit">
             <i className="icon-clone" />
-          </div>
+          </Link>
         </div>
       </div>
 
       <Widget config={widgetConfig} />
-
-      {widgetConfig && (
-        <div className="mt-4 link delay">
-          <Link to={`/widget/new?fromId=${widgetId}`}>
-            Create a new Widget by editing this
-          </Link>
-        </div>
-      )}
 
     </div>
   );
