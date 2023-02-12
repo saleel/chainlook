87a9431f622c87f572fcdae87062b0957602d9b8
Saleel <saleel@me.com>
2023-02-13 01:25:25 +0530

    feat: implement dashboard page

--- src/pages/dashboard-page.tsx
+++ src/pages/dashboard-page.tsx
@@ -1,18 +1,24 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import DashboardView from '../components/dashboard-view';
-import API from '../data/api';
-import usePromise from '../hooks/use-promise';
+import React from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import DashboardView from "../components/dashboard-view";
+import { AuthContext } from "../context/auth-context";
+import API from "../data/api";
+import usePromise from "../hooks/use-promise";
 
 function DashboardPage() {
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const [dashboard, { isFetching, error }] = usePromise(() => API.getDashboard(id as string), {
-    dependencies: [id],
-    conditions: [id],
-  });
+  const { user } = React.useContext(AuthContext);
+
+  const [dashboard, { isFetching, error }] = usePromise(
+    () => API.getDashboard(id as string),
+    {
+      dependencies: [id],
+      conditions: [id],
+    }
+  );
 
   React.useEffect(() => {
     if (dashboard) {
@@ -26,24 +32,46 @@ function DashboardPage() {
   // }, [dashboard]);
 
   if (isFetching) {
-    return (<div className="page dashboard-page">Loading</div>);
+    return <div className="page dashboard-page">Loading</div>;
   }
 
   if (error) {
-    return (<div className="page dashboard-page">{error.message}</div>);
+    return <div className="page dashboard-page">{error.message}</div>;
   }
+
+  const isDashboardOwner = dashboard?.user?.id === user?.id;
 
   return (
     <div className="page dashboard-page">
-
       <div className="dashboard-header">
-        <h2 className="dashboard-title">
-          {dashboard?.title}
-        </h2>
+        <div className="dashboard-title">
+          <h2>{dashboard?.title}</h2>
+
+          <div className="dashboard-info mt-1">
+            {dashboard.user && (
+              <span className="tag mr-2">👤 {dashboard.user?.username}</span>
+            )}
+
+            {dashboard.tags?.map((tag: string) => (
+              <span className="tag mr-2">#{tag}</span>
+            ))}
+          </div>
+        </div>
+
+        <div className="dashboard-actions">
+          {isDashboardOwner && (
+            <Link
+              className="button is-normal"
+              to={`/dashboards/${dashboard.id}/edit`}
+              title="Edit the dashboard"
+            >
+              Edit
+            </Link>
+          )}
+        </div>
       </div>
 
       <DashboardView dashboard={dashboard} />
-
     </div>
   );
 }
