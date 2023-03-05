39c261a27fea28956d93a1f556bfe65f393bbe19
Saleel <saleel@me.com>
2023-03-05 12:45:12 +0530

    chore: remove cache from usePromise

--- src/hooks/use-promise.ts
+++ src/hooks/use-promise.ts
@@ -1,82 +1,22 @@
-// @ts-check
-
 import React from 'react';
-import differenceInSeconds from 'date-fns/differenceInSeconds';
-
-const cache = {
-  get(key) {
-    if (!key) return undefined;
-
-    let cachedData;
-
-    try {
-      cachedData = window.localStorage.getItem(`cache.${key}`);
-      if (cachedData) {
-        cachedData = JSON.parse(cachedData);
-      }
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error('Cannot read cache', error);
-    }
-
-    return cachedData;
-  },
-
-  set(key, data) {
-    const newDoc = {
-      data,
-      storedAt: new Date(),
-    };
-
-    try {
-      window.localStorage.setItem(`cache.${key}`, JSON.stringify(newDoc));
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error('Cannot set cache', err);
-    }
-  },
-
-  delete(key) {
-    window.localStorage.removeItem(`cache.${key}`);
-  },
-};
 
 type UsePromiseOptions = {
   defaultValue?: any;
   dependencies?: any[];
   conditions?: any[];
-  cacheKey?: string;
-  updateWithRevalidated?: boolean;
-  cachePeriodInSecs?: number;
 };
 
 function usePromise<T>(promise: () => Promise<T>, options: UsePromiseOptions = {}) {
-  const {
-    defaultValue,
-    dependencies = [],
-    cacheKey,
-    updateWithRevalidated = true,
-    cachePeriodInSecs = 10,
-    conditions = [],
-  } = options;
-
-  let cachedData: any;
-  if (cacheKey) {
-    cachedData = cache.get(cacheKey);
-  }
+  const { defaultValue, dependencies = [], conditions = [] } = options;
 
   const allConditionsValid = conditions.every((condition) => {
     if (typeof condition === 'function') return !!condition();
     return !!condition;
   });
 
-  const [result, setResult] = React.useState<T>(cachedData ? cachedData.data : defaultValue);
-  const [fetchedAt, setFetchedAt] = React.useState<Date>(
-    cachedData ? cachedData.storedAt : undefined,
-  );
-  const [isFetching, setIsFetching] = React.useState<boolean>(
-    allConditionsValid && !(cachedData && cachedData.data),
-  );
+  const [result, setResult] = React.useState<T>(defaultValue);
+  const [fetchedAt, setFetchedAt] = React.useState<Date>();
+  const [isFetching, setIsFetching] = React.useState<boolean>(allConditionsValid);
   const [error, setError] = React.useState<Error>();
 
   let didCancel = false;
@@ -87,38 +27,19 @@ function usePromise<T>(promise: () => Promise<T>, options: UsePromiseOptions = {
       setError(undefined);
     }
 
-    if (cachedData) {
-      if (
-        cachedData.storedAt &&
-        differenceInSeconds(new Date(), new Date(cachedData.storedAt)) < cachePeriodInSecs
-      ) {
-        return;
-      }
-    }
-
     setIsFetching(true);
 
     try {
       const data = await promise();
       if (!didCancel) {
-        // In some cases newly fetched data don't have to be updated (updateWithRevalidated = false)
-        if (updateWithRevalidated || cachedData === undefined) {
-          setResult(data);
-          setFetchedAt(new Date());
-        }
-
-        if (cacheKey && (data !== null || data !== undefined)) {
-          cache.set(cacheKey, data);
-        }
+        setResult(data);
+        setFetchedAt(new Date());
       }
     } catch (e) {
       if (!didCancel) {
         // eslint-disable-next-line no-console
         console.error('Error on fetching data', e);
         setError(e as Error);
-        // if (cacheKey) {
-        //   cache.delete(cacheKey);
-        // }
       }
     }
 
@@ -137,10 +58,6 @@ function usePromise<T>(promise: () => Promise<T>, options: UsePromiseOptions = {
   }, dependencies);
 
   function reFetch() {
-    if (cacheKey) {
-      cache.delete(cacheKey);
-      cachedData = undefined;
-    }
     return fetch();
   }
 
